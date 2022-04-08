36d057a7701e01666dfdcba3383c4a2fc6eacbeb
lijie <1821378008@qq.com>
2022-04-08 10:18:55 +0800

    改变代码，使用Timer 实现

--- app/src/main/java/com/example/mytestdemo/MainActivity.java
+++ app/src/main/java/com/example/mytestdemo/MainActivity.java
@@ -1,54 +1,37 @@
 package com.example.mytestdemo;
-
-
 import androidx.annotation.NonNull;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-
 import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-
 import android.os.Bundle;
 import android.os.Handler;
-
 import android.os.Message;
-import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.WeakHashMap;
 
-public class MainActivity extends AppCompatActivity {
-    PendingIntent pendingIntent;
-    long start = SystemClock.elapsedRealtime() + 1000 * 10;
-    long repeat = 5 * 1000;
-    AlarmManager alarmManager;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textView;
-    IntentFilter intentFilter;
-    myReceiver myReceiver = new myReceiver();
-
-    private Handler handler = new Handler() {
+    Timer timer = new Timer();
+    Handler handler = new Handler(){
         @Override
         public void handleMessage(@NonNull Message msg) {
-            switch (msg.what) {
-                case 1:
+                switch (msg.what){
+                    case 1:
+                        if(textView.getText().equals("现在是晚上")){
+                            textView.setText("现在是早上");
+                        }else{
+                            textView.setText("现在是晚上");
+                        }
+                }
 
-                    Intent intent1 = new Intent("android.example.change");
-                    alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                    pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent1, PendingIntent.FLAG_NO_CREATE);
-                    sendBroadcast(intent1);
-                    break;
-            }
         }
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,72 +39,28 @@ public class MainActivity extends AppCompatActivity {
         Button start = findViewById(R.id.start);
         Button end = findViewById(R.id.end);
         textView = findViewById(R.id.tv);
+        start.setOnClickListener(this);
+        end.setOnClickListener(this);
+    }
 
 
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intentFilter = new IntentFilter("android.example.change");
-                registerReceiver(myReceiver, intentFilter);
-
-                new Thread(new Runnable() {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.start:
+                timer.schedule(new TimerTask() {
                     @Override
                     public void run() {
-                        try {
-                            Thread.sleep(1000 * 10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
                         Message message = new Message();
                         message.what = 1;
                         handler.sendMessage(message);
                     }
-                }).start();
+                },1000*10,1000*5);
 
+                break;
 
-            }
-        });
-        end.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                alarmManager.cancel(pendingIntent);
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(myReceiver);
-
-    }
-
-    public class myReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, start, repeat, pendingIntent);
-
-                            Toast.makeText(MainActivity.this, "到了这里", Toast.LENGTH_SHORT).show();
-                            if (textView.getText().equals("现在是晚上")) {
-                                textView.setText("现在是早上");
-                            } else {
-                                textView.setText("现在是晚上");
-                            }
-                        }
-                    });
-                }
-            }).start();
-
+            default:
+                break;
         }
     }
-
-
 }
